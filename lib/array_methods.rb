0455294b3e76ac43f307fffd82d008f3c429cffc
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-06 21:00:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning","arrays!"]
-if array.include?("wow")
+if element.include?("wow")
   return true 
 else
   return false
